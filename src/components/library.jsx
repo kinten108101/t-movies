5e0893cc43ca7d4432c0a54870962efabfad5241
unknown <timzek7@gmail.com>
2022-10-25 20:57:28 +0700

    Change home page style a bit

--- src/components/library.jsx
+++ src/components/library.jsx
@@ -1,64 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { CardgridCard } from "./cardgrid.jsx";
 import "../styles/main.css";
+import { MOVIE_DETAILS_URL } from "../api.js";
+import axios from "axios";
+
+const MovieDetails = (props) => {
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${MOVIE_DETAILS_URL}/${props.id}`)
+      .then((res) => {
+        setMovie(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, [props.id]);
+  return (
+    <CardgridCard
+      id={movie?.id}
+      tv_img={movie?.image?.medium}
+      tv_name={movie?.name}
+    ></CardgridCard>
+  );
+};
 
 const MvLibrary = () => {
+  const featured_movie = [169, 618, 48945, 28276, 40329, 1371, 305, 269, 82];
+
   return (
     <div className="mainview">
-      <CardgridCard
-        id="169"
-        tv_name="Breaking Bad"
-        tv_display_name="true"
-        tv_img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg2.looper.com%2Fimg%2Fgallery%2Fthe-best-episodes-of-breaking-bad%2Fl-intro-1609890074.jpg&f=1&nofb=1&ipt=e44622c7354acca258e41d466df523634feef83b24808c8ab8cbfc1bac5f3afe&ipo=images"
-      />
-      <CardgridCard
-        id="618"
-        tv_name="Better Call Saul"
-        tv_display_name="true"
-        tv_img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic3.srcdn.com%2Fwordpress%2Fwp-content%2Fuploads%2F2020%2F04%2FBetter-Call-Saul.jpg&f=1&nofb=1&ipt=cfe7c2a1b72380097ad0b9e474233567cc392c4e189498e54c85206178792d2d&ipo=images"
-      />
-      <CardgridCard
-        id="48945"
-        tv_name="Cyberpunk: Edgerunners"
-        tv_display_name="true"
-        tv_img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.wallpapersden.com%2Fimage%2Fdownload%2Fbackground-of-cyberpunk-edgerunners_71883_480x800.jpg&f=1&nofb=1&ipt=6febe992fb01748a6dec93e0ad94af4ce7d94b81a75539212b344c3ff35405dc&ipo=images"
-      />
-      <CardgridCard
-        id="28276"
-        tv_name="The Witcher"
-        tv_display_name="true"
-        tv_img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.blogs.es%2F17636d%2Fthe-witcher%2F450_1000.jpeg&f=1&nofb=1&ipt=a238aed350ca62d17bcfb399cec17ff5e831e520c6cc0967857ab000dc874c06&ipo=images"
-      />
-      <CardgridCard
-        id="40329"
-        tv_name="Love, Death and Robots"
-        tv_display_name="true"
-        tv_img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstreaming-engine-assets.rftslb.com%2Fposters%2Fimage%2Fimage%2F881435%2Flist_page_main.jpg&f=1&nofb=1&ipt=1e66ff27adacc7abc98d4214143c41b8d47b920a9aa4e8b3b4ea07c06dbe333e&ipo=images"
-      />
-      <CardgridCard
-        id="1371"
-        tv_name="Westworld"
-        tv_display_name="true"
-        tv_img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.gadgets360cdn.com%2Flarge%2Fwestworld-season-3-poster_1582269939846.jpg&f=1&nofb=1&ipt=cbb3f4791d1004c93c7c9b4e7c5812b836df0ac4a07d924ed4777e8269403929&ipo=images"
-      />
-      <CardgridCard
-        id="305"
-        tv_name="Black Mirror"
-        tv_display_name="true"
-        tv_img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmamasgeeky.com%2Fwp-content%2Fuploads%2F2019%2F06%2Fblack-mirror-season-5-striking-vipers-poster.jpg&f=1&nofb=1&ipt=f8de052fdd44b26c7a3ebd57235b97649d959c1fc11c27e8ab307addcb655dd8&ipo=images"
-      />
-      <CardgridCard
-        id="24532"
-        tv_name="El Camino: a Breaking Bad movie"
-        tv_display_name="true"
-        tv_img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.themoviedb.org%2Ft%2Fp%2Foriginal%2Fw4TZShtZJXzOujWxa1Je8O1g87L.jpg&f=1&nofb=1&ipt=0269943cbd6e24fc5525e336858cec9feb115d9b4984f67ac331aa73d3056e2a&ipo=images"
-      />
-      <CardgridCard
-        id="82"
-        tv_name="Game Of Thrones"
-        tv_display_name="true"
-        tv_img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimagesvc.timeincapp.com%2Fv3%2Ffan%2Fimage%3Furl%3Dhttps%3A%2F%2Fwinteriscoming.net%2Fwp-content%2Fblogs.dir%2F385%2Ffiles%2F2019%2F02%2Fmgot_s8_character_art_dany.jpg%26c%3Dsc%26w%3D844%26h%3D1284&f=1&nofb=1&ipt=fce1d1f729a63513cdff37b062e6aa4bbfab01397900a9c439fa8396b7163fb7&ipo=images"
-      />
+      {featured_movie?.map((idphim) => (
+        <MovieDetails id={idphim} />
+      ))}
     </div>
   );
 };
